1dc2b7b2fd1219d153ed3b14be4bf04d47664ccb
wizerwings <wizerwings@gmail.com>
2021-02-13 10:01:10 +0100

    Checking parameters when adding priority in add() method and sending ResponseEntity

--- src/main/java/com/romansholokh/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/com/romansholokh/tasklist/backendspringboot/controller/PriorityController.java
@@ -2,6 +2,9 @@ package com.romansholokh.tasklist.backendspringboot.controller;
 
 import com.romansholokh.tasklist.backendspringboot.entity.Priority;
 import com.romansholokh.tasklist.backendspringboot.repo.PriorityRepository;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,8 +29,23 @@ public class PriorityController
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority)
+    public ResponseEntity<Priority> add(@RequestBody Priority priority)
     {
-        return priorityRepository.save(priority);
+        if(priority.getId() != null && priority.getId() != 0)
+        {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        else if (priority.getTitle() == null || priority.getTitle().trim().length() == 0)
+        {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        else if (priority.getColor() == null
+                || priority.getColor().trim().length() == 0
+                || !(priority.getColor().startsWith("#")))
+        {
+            return new ResponseEntity("missed param or invalid format: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
